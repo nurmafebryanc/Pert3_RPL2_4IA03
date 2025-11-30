81ab99f3c023ac52e78d73880ce1c7f05d8707cf
unknown <nurmafebryanc@gmail.com>
2025-11-30 15:49:32 +0700

    Commit Pertemuan 3

---
+++ src/main/java/com/mycompany/mahasiswa_51422267/Mahasiswa_51422267.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa_51422267;
+
+/**
+ *
+ * @author Nurma
+ */
+public class Mahasiswa_51422267 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
